e8a9a5d7c21c9f96725e3e31a91c73179b906a26
Abhishek Gupta <abhishekgupta07@yahoo.com>
2021-07-31 21:50:42 -0700

    Update API.js

--- src/apis/API.js
+++ src/apis/API.js
@@ -2,4 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: 'http://eacodingtest.digital.energyaustralia.com.au/api/v1'
+//   baseURL: 'https://eacp.energyaustralia.com.au/codingtest/api/v1/festivals'
 });
